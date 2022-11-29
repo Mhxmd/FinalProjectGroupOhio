f549978f9b7b4b2e6d0fea2876244ce6e6095059
Mhxmd <m.muslan@yahoo.com>
2022-11-29 10:52:49 +0800

    v1.0

---
+++ TestMyProgram.py
@@ -0,0 +1,12 @@
+import unittest
+import myProgram as prog
+class TestMyProgram(unittest.TestCase):
+
+    def test_EngineType(self):
+        vios = prog.Vehicle('4', 'petrol', 5, 180)
+        self.assertEqual(vios.type_of_tank, 'petrol')
+
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        unittest.main()
+
